ee5e86434e9a8146cb2aa2479a01ece22aa1996f
sdennruss <sdennisrussell@gmail.com>
2020-04-10 22:14:10 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className="container-1">
+        <div className="box-1">
+          <h3>Box One</h3>
+          <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???
+          </p>
+        </div>
+        <div className="box-2">
+          <h3>Box Two</h3>
+          <p>This is box two! </p>
+        </div>
+        <div className="box-3">
+          <h3>Box Three</h3>
+          <p>This is box three!</p>
+        </div>
+      </div>
+      
+    <div className="container-2">
+      <div className="container-2-box">
+        <h3>Box Four</h3>
+        <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="container-2-box">
+        <h3>Box Five</h3>
+        <p>This is box two! </p>
+      </div>
+      <div className="container-2-box">
+        <h3>Box Six</h3>
+        <p>This is box three!</p>
+      </div>
     </div>
-  );
+
+    <div className="container-3">
+      <div className="container-3-box">
+        <h3>Box Seven</h3>
+        <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???
+        </p>
+      </div>
+      <div className="container-3-box">
+        <h3>Box Eight</h3>
+        <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???</p>
+      </div>
+      <div className="container-3-box">
+        <h3>Box Nine</h3>
+        <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???</p>
+      </div>
+      <div className="container-3-box">
+      <h3>Box Ten</h3>
+      <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???
+      </p>
+    </div>
+    <div className="container-3-box">
+      <h3>Box Eleven</h3>
+      <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now??? </p>
+    </div>
+    <div className="container-3-box">
+      <h3>Box Twelve</h3>
+      <p>This is box one!Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???Is there anyone home right now???</p>
+    </div>
+    </div>
+  </div>
+  )
 }
 
 export default App;
