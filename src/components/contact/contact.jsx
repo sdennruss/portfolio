733f62a44f4eef448c21a95eab0fce10d54ba251
sdennruss <sdennisrussell@gmail.com>
2020-09-20 09:10:49 -0400

    edits on mobile

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -15,11 +15,10 @@ const Contact = () => {
 
         <div className="contact-right">
           <p className="contact-paragraph">
-            {" "}
             One of my priorities is to focus on working with ethical brands that
             offer products and/or services without the harm of animals or
             humans. Have a project you want to get started on? Connect via email
-            and let's talk details.
+            and let's talk details: info@shanicedr.com
           </p>
 
           <p className="ethical">eth·i·cal</p>
