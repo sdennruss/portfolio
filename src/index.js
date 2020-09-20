54003fdf3bb00825530575a3204ab8e386ba59f7
sdennruss <sdennisrussell@gmail.com>
2020-09-20 02:57:11 -0400

    new

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import "font-awesome/css/font-awesome.css";
-import "./App.css";
 
 ReactDOM.render(
   <BrowserRouter>
