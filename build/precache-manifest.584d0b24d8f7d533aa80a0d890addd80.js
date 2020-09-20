5fbc9e8ef5c29ce3f0f751ba069e53af17aaa0fc
sdennruss <sdennisrussell@gmail.com>
2020-09-20 03:12:39 -0400

    adding build folder on repo

---
+++ build/precache-manifest.584d0b24d8f7d533aa80a0d890addd80.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38faa8d4254490305b56bd790aa8871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42f338f6f2e04b768dfb",
+    "url": "/static/css/2.9a9b1a2c.chunk.css"
+  },
+  {
+    "revision": "7fdbe99f72d6a207ee9a",
+    "url": "/static/css/main.a5d3cd33.chunk.css"
+  },
+  {
+    "revision": "42f338f6f2e04b768dfb",
+    "url": "/static/js/2.0fde24d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0fde24d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdbe99f72d6a207ee9a",
+    "url": "/static/js/main.a27c9fbe.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "30262dfafce59b911b9e6cc895e96b5b",
+    "url": "/static/media/cow1.30262dfa.png"
+  },
+  {
+    "revision": "30262dfafce59b911b9e6cc895e96b5b",
+    "url": "/static/media/cow2.30262dfa.png"
+  },
+  {
+    "revision": "30262dfafce59b911b9e6cc895e96b5b",
+    "url": "/static/media/cow3.30262dfa.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a906386687bb54e9cc218ddb34413d58",
+    "url": "/static/media/photoshoot.a9063866.jpg"
+  },
+  {
+    "revision": "10e04c3df4a7149bcd52ba0a12088646",
+    "url": "/static/media/scooter-mock.10e04c3d.png"
+  }
+]);
